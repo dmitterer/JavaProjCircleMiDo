a507776994e32eae25d79028bf74900d98289eb5
Dominik Mitterer <dmitterer@tsn.at>
2021-09-28 19:06:55 +0200

    Feature2 Added

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,9 @@ public class Main {
         coor.set(13,4);
         System.out.println(circle.toString());
     }
+    public  void Test12(){
+
+        System.out.println("Test Feature 2");
+
+    }
 }
